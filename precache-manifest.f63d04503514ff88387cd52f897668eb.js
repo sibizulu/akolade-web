59fa858838f20a20e8a13ba7b5fd25d9a80a4be8
Sibiraj PR <sibiraj@lightrains.com>
2018-10-29 14:38:36 +0530

    Adding new build

---
+++ precache-manifest.f63d04503514ff88387cd52f897668eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc4abcb0117c5f29f1b1",
+    "url": "/akolade-web/static/js/runtime~main.dc4abcb0.js"
+  },
+  {
+    "revision": "1e70d756cd06377d9652",
+    "url": "/akolade-web/static/js/main.1e70d756.chunk.js"
+  },
+  {
+    "revision": "0626cb85519d1017015e",
+    "url": "/akolade-web/static/js/1.0626cb85.chunk.js"
+  },
+  {
+    "revision": "1e70d756cd06377d9652",
+    "url": "/akolade-web/static/css/main.a886b721.chunk.css"
+  },
+  {
+    "revision": "0626cb85519d1017015e",
+    "url": "/akolade-web/static/css/1.3a25093a.chunk.css"
+  },
+  {
+    "revision": "5a52b1907449a833aaf745696ec710a3",
+    "url": "/akolade-web/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f041e78f7f0571e0b81b19a1a91a92a9.js"
+  "/akolade-web/precache-manifest.f63d04503514ff88387cd52f897668eb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/akolade-web/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
